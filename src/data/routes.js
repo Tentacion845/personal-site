0f08ee26e80973feefcebdc009a9d27f2ac87056
Said <sladghem@norauto.com>
2023-04-30 22:28:00 +0200

    init

---
+++ src/data/routes.js
@@ -0,0 +1,23 @@
+const routes = [
+  {
+    index: true,
+    label: 'Hadel Ladghem',
+    path: '/',
+  },
+  {
+    label: 'About',
+    path: '/about',
+  },
+
+  {
+    label: 'Projects',
+    path: '/projects',
+  },
+
+  {
+    label: 'Contact',
+    path: '/contact',
+  },
+];
+
+export default routes;
